87c37111e962595284f0beaacdad18b5b7aa85c6
Lauren Glegg <lauren@hifyre.com>
2020-10-08 11:54:46 -0400

    Initial commit

---
+++ src/js/components/Example.vue
@@ -0,0 +1,25 @@
+<template>
+  <section>
+    <div class="container">
+      <img src="/assets/img/logo.png" :alt="message" />
+      <p>{{ message }}</p>
+    </div>
+  </section>
+</template>
+
+<script>
+  export default {
+    data() {
+      return {
+        message: 'Example Vue component'
+      }
+    }
+  }
+</script>
+
+<style lang="scss" scoped>
+  img {
+    max-width: 100px;
+    margin-top: 3rem;
+  }
+</style>
